a9f049f9598a0d4d646e7f01ec61beba2c52bb1b
spf <2215486567@qq.com>
2019-06-23 21:22:16 +0800

    xx

---
+++ day1/printGame1.py
@@ -0,0 +1,9 @@
+print('---------------------------I LOVE JAVA--------------------------')
+temp = input("猜猜数字:")
+guess = int(temp)
+if guess == 8:
+    print("牛批")
+    print("但没什么卵用")
+else:
+    print("嘻嘻，你猜错的样子像CXK")
+print("游戏结束，不玩啦")
